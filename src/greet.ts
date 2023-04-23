c331defc8f8f7b93ce9d1d2624fb6c5a23a62fc6
Orta Therox <git@orta.io>
2023-04-23 18:55:21 +0100

    More initial green

--- src/greet.ts
+++ src/greet.ts
@@ -3,11 +3,7 @@ import { GreetOptions } from "./types.js";
 const consoleLogBound = console.log.bind(console);
 
 export function greet(options: GreetOptions | string) {
-	const {
-		logger = consoleLogBound,
-		message,
-		times = 1,
-	} = typeof options === "string" ? { message: options } : options;
+	const { logger = consoleLogBound, message, times = 1 } = typeof options === "string" ? { message: options } : options;
 
 	for (let i = 0; i < times; i += 1) {
 		logger(message);
