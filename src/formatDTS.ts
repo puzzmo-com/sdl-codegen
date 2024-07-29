4563a652c69c9e6cb386ec13765bc20b6d4eb321
Orta Therox <git@orta.io>
2024-07-29 19:20:48 +0100

    Update prettier fns

--- src/formatDTS.ts
+++ src/formatDTS.ts
@@ -1,9 +1,8 @@
+/* eslint-disable */
 // https://prettier.io/docs/en/api.html
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 let prettier: any | null = null
 try {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	prettier = await import("prettier")
 } catch (er) {
 	console.error(er)
@@ -11,16 +10,20 @@ try {
 }
 
 export const getPrettierConfig = (path: string): unknown => {
-	if (!prettier) return {}
+	if (!prettier?.default?.resolveConfig) return {}
+	if (typeof prettier.default.resolveConfig !== "function") return {}
 
-	// I confirmed that this lookup hits caches in Redwood
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+	// I confirmed that this lookup hits caches in RedwoodJS
 	const opts = prettier.default.resolveConfig.sync(path) ?? {}
 	return opts
 }
 
 export const formatDTS = (path: string, content: string, config: unknown): string => {
-	if (!prettier) return content
+	if (!prettier?.default?.format) return content
+	if (typeof prettier.default.format !== "function") return content
+
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 	return prettier.default.format(content, { ...(config as object), filepath: path }) as string
 }
+
+/* eslint-enable */
